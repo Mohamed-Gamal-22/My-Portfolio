51d5649f4ddae0e834ce6d3e3e952ef2f168f5d5
mohamed gamal <mogamall086@gmail.com>
2023-02-27 11:02:49 +0200

    fix footer view

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
   return (
     <div className='mt-4 w-100 bg-dark text-white text-capitalize p-3'>
         <div className="container ">
-            <div className=' w-100 h-100 d-flex justify-content-between align-items-center'>
+            <div className={`w-100 d-flex justify-content-between align-items-center ${style.foot}`}>
                 <p className='mb-0'>all rights reserved <span className='fw-bold text-danger'>mohamed gamal</span> &reg;  & copyright <span className='text-danger'>&copy;</span> March 2023</p>
                 <p onClick={() => backToTop()} className={`position-relative mb-0 clc ${style.clc}`}>back to top <i className={`fa-solid fa-arrow-up ${style.arr}`}></i></p>
             </div>
